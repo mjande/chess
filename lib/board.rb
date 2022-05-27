d21a46cf64cc7ac798ffa72f58d525d6cc4c9253
mattand <mjande@umich.edu>
2022-05-26 20:30:51 -0400

    Modify #display

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 require_relative 'pieces'
+require 'colorize'
 
 class Board
   attr_reader :position_array
@@ -7,10 +8,30 @@ class Board
     @position_array = Array.new(8) { Array.new(8, nil) }
   end
 
+  def add_background_color(array)
+    array.map.with_index do |row, row_index|
+      row.map.with_index do |position, column_index|
+        if row_index.even?
+          if column_index.even?
+            position.colorize(background: :light_white)
+          else
+            position.colorize(background: :light_blue)
+          end
+        elsif row_index.odd?
+          if column_index.even?
+            position.colorize(background: :light_blue)
+          else
+            position.colorize(background: :light_white)
+          end
+        end
+      end
+    end
+  end
+
   def display
     horizontal_line = '  --------------------------------- '
     column_labels = '    a   b   c   d   e   f   g   h   '
-
+    
     printable_board = clean_rows.join("#{horizontal_line}\n")
 
     puts horizontal_line
@@ -21,8 +42,10 @@ class Board
 
   def clean_rows
     clean_rows = position_array.map do |row|
-      row.map { |position| position.nil? ? ' ' : position }
+      row.map { |position| position.nil? ? '   ' : position }
     end
+
+    clean_rows = add_background_color(clean_rows)
 
     row_number = 8
     clean_rows.each do |row|
@@ -31,6 +54,6 @@ class Board
       row_number -= 1
     end
 
-    clean_rows.map { |row| row.join(' | ') }
+    clean_rows.map { |row| row.join('|') }
   end
 end
--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Piece
   def initialize(row, column)
     @row = row
@@ -9,10 +11,10 @@ class Piece
     case color
     when 'white'
       starting_positions = self::WHITE_STARTING_POSITIONS
-      symbol = self::WHITE_SYMBOL
+      symbol = self::SYMBOL.colorize(:white)
     when 'black'
       starting_positions = self::BLACK_STARTING_POSITIONS
-      symbol = self::BLACK_SYMBOL
+      symbol = self::SYMBOL.colorize(:black)
     end
 
     starting_positions.each do |position|
@@ -28,45 +30,40 @@ end
 
 class WhitePawn < Piece
   WHITE_STARTING_POSITIONS = [[6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7]].freeze
-  WHITE_SYMBOL = '♙'.freeze
+  SYMBOL = ' ♟ '.freeze
 end
 
 class BlackPawn < Piece
   BLACK_STARTING_POSITIONS = [[1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]].freeze
-  BLACK_SYMBOL = '♟'.freeze
+  SYMBOL = ' ♟ '.freeze
 end
 
 class Rook < Piece
   WHITE_STARTING_POSITIONS = [[7, 0], [7, 7]].freeze
-  WHITE_SYMBOL = '♖'.freeze
   BLACK_STARTING_POSITIONS = [[0, 0], [0, 7]].freeze
-  BLACK_SYMBOL = '♜'.freeze
+  SYMBOL = ' ♜ '.freeze
 end
 
 class Knight < Piece
   WHITE_STARTING_POSITIONS = [[7, 1], [7, 6]].freeze
-  WHITE_SYMBOL = '♘'.freeze
   BLACK_STARTING_POSITIONS = [[0, 1], [0, 6]].freeze
-  BLACK_SYMBOL = '♞'.freeze
+  SYMBOL = ' ♞ '.freeze
 end
 
 class Bishop < Piece
   WHITE_STARTING_POSITIONS = [[7, 2], [7, 5]].freeze
-  WHITE_SYMBOL = '♗'.freeze
   BLACK_STARTING_POSITIONS = [[0, 2], [0, 5]].freeze
-  BLACK_SYMBOL = '♝'.freeze
+  SYMBOL = ' ♝ '.freeze
 end
 
 class Queen < Piece
   WHITE_STARTING_POSITIONS = [[7, 3]].freeze
-  WHITE_SYMBOL = '♕'.freeze
   BLACK_STARTING_POSITIONS = [[0, 3]].freeze
-  BLACK_SYMBOL = '♛'.freeze
+  SYMBOL = ' ♛ '.freeze
 end
 
 class King < Piece
   WHITE_STARTING_POSITIONS = [[7, 4]].freeze
-  WHITE_SYMBOL = '♔'.freeze
   BLACK_STARTING_POSITIONS = [[0, 4]].freeze
-  BLACK_SYMBOL = '♚'.freeze
+  SYMBOL = ' ♚ '.freeze
 end
