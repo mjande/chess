2729d326a6f24c079caae5d0a4e99b0630e629ef
mattand <mjande@umich.edu>
2022-05-29 07:47:06 -0400

    Finish #possible_moves for Rook class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -2,11 +2,12 @@ require 'colorize'
 
 class Piece
   attr_reader :color
-  
-  def initialize(row, column, color)
+
+  def initialize(row, column, color, board)
     @row = row
     @column = column
     @color = color
+    board.positions[row][column] = self
   end
 
   def self.add_to_board(color, board)
@@ -21,9 +22,8 @@ class Piece
     starting_positions.each do |position|
       row = position[0]
       column = position[1]
-      piece = new(row, column, color)
+      piece = new(row, column, color, board)
       piece_set << piece
-      board.positions[row][column] = piece
     end
     piece_set
   end
@@ -59,6 +59,8 @@ class Rook < Piece
     possible_moves_array = []
     check_up(board).each { |position| possible_moves_array << position }
     check_right(board).each { |position| possible_moves_array << position }
+    check_down(board).each { |position| possible_moves_array << position }
+    check_left(board).each { |position| possible_moves_array << position }
     possible_moves_array
   end
 
@@ -67,8 +69,12 @@ class Rook < Piece
   def check_up(board)
     moves_up = []
     next_row = @row - 1
-    while board.positions[next_row][@column].nil? && next_row.between?(0, 7)
-      moves_up << [next_row, @column]
+    while next_row >= 0
+      if board.positions[next_row][@column].nil? || board.positions[next_row][@column].color != color
+        moves_up << [next_row, @column]
+      end
+      break unless board.positions[next_row][@column].nil?
+
       next_row -= 1
     end
     moves_up
@@ -77,11 +83,43 @@ class Rook < Piece
   def check_right(board)
     moves_right = []
     next_column = @column + 1
-    while board.positions[@row][next_column].nil? && next_column.between?(0, 7)
-      moves_right << [@row, next_column]
+    while next_column <= 7
+      if board.positions[@row][next_column].nil? || board.positions[@row][next_column].color != color
+        moves_right << [@row, next_column]
+      end
+      break unless board.positions[@row][next_column].nil?
+
       next_column += 1
     end
     moves_right
+  end
+
+  def check_down(board)
+    moves_down = []
+    next_row = @row + 1
+    while next_row <= 7
+      if board.positions[next_row][@column].nil? || board.positions[next_row][@column].color != color
+        moves_down << [next_row, @column]
+      end
+      break unless board.positions[next_row][@column].nil?
+
+      next_row += 1
+    end
+    moves_down
+  end
+
+  def check_left(board)
+    moves_left = []
+    next_column = @column - 1
+    while next_column >= 0
+      if board.positions[@row][next_column].nil? || board.positions[@row][next_column].color != color
+        moves_left << [@row, next_column]
+      end
+      break unless board.positions[@row][next_column].nil?
+
+      next_column -= 1
+    end
+    moves_left
   end
 end
 
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -31,11 +31,32 @@ describe Rook do
     let(:positions) { Array.new(8) { Array.new(7, nil) } }
     let(:board) { double('board', positions: positions) }
 
-    it 'returns an array of all possible moves on a blank board' do
-      a_rook = Rook.new(7, 0, 'white')
-      board.positions[7][0] = a_rook
-      possible_moves_array = [[6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7]]
-      expect(a_rook.possible_moves(board)).to eq(possible_moves_array)
+    context 'when the board is blank' do
+      it 'returns an array of all possible moves' do
+        a_rook = Rook.new(7, 0, 'white', board)
+        expect(a_rook.possible_moves(board)).to contain_exactly([6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7])
+      end
+
+      it 'returns an array of all possible moves' do
+        h_rook = Rook.new(0, 7, 'black', board)
+        expect(h_rook.possible_moves(board)).to contain_exactly([0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7])
+      end
+    end
+
+    context 'there are other pieces on the board' do
+      it 'returns moves up to blocking pieces and including different colored pieces' do
+        a_rook = Rook.new(7, 0, 'white', board)
+        Bishop.new(7, 2, 'white', board)
+        BlackPawn.new(1, 0, 'black', board)
+        expect(a_rook.possible_moves(board)).to contain_exactly([7, 1], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0])
+      end
+
+      it 'returns moves up to blocking pieces and including different colored pieces' do
+        h_rook = Rook.new(0, 7, 'black', board)
+        Queen.new(0, 3, 'black', board)
+        Bishop.new(5, 7, 'white', board)
+        expect(h_rook.possible_moves(board)).to contain_exactly([1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [0, 6], [0, 5], [0, 4])
+      end
     end
   end
 end
