ffaa5ce32159295c77b02ccc5bd86186bfbd0123
mattand <mjande@umich.edu>
2022-06-02 20:24:47 -0400

    Add initial #check and #checkmate methods

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@ require_relative 'library'
 
 class Game
   attr_reader :board, :white_player, :black_player, :white_pieces, :black_pieces
+
   def start_game
     @board = Board.new
     @white_player = Player.new('white', board)
@@ -9,6 +10,14 @@ class Game
     add_white_pieces_to_board
     add_black_pieces_to_board
     play_game
+  end
+
+  def white_king
+    white_pieces.find { |piece| piece.instance_of?(King) }
+  end
+
+  def black_king
+    black_pieces.find { |piece| piece.instance_of?(King) }
   end
 
   def add_white_pieces_to_board
@@ -38,8 +47,11 @@ class Game
   def play_game
     until checkmate?
       play_turn(white_player)
+      black_player.check_message if check?(black_king)
       break if checkmate?
+
       play_turn(black_player)
+      white_player.check_message if check?(white_king)
     end
     end_game
   end
@@ -51,36 +63,51 @@ class Game
       move = player.input_move
       selected_piece = pieces.find do |piece|
         piece.instance_of?(move[0]) &&
-        piece.possible_moves.include?([move[1], move[2]])
+          piece.possible_moves.include?([move[1], move[2]])
       end
       selected_piece.move(move[1], move[2])
-      break unless check? == player.color
+      break unless check?(pieces.find { |piece| piece.instance_of?(King) })
 
       puts 'Illegal move. Choose a different move.'
       selected_piece.undo_move
     end
-    player.check_message(check?) if check?
   end
 
-  def check?
-    white_king = white_pieces.find { |piece| piece.instance_of?(King) }
-    black_king = black_pieces.find { |piece| piece.instance_of?(King) }
-    if black_pieces.any? do |piece|
-      piece.possible_moves.include?([white_king.row, white_king.column])
+  def check?(king)
+    pieces = (king.color == 'white' ? black_pieces : white_pieces)
+    pieces.any? do |piece|
+      piece.possible_moves.include?([king.row, king.column])
+    end
+  end
+
+  def checkmate?
+    if white_king.possible_moves.all? do |position|
+      black_pieces.any? do |piece|
+        piece.possible_moves.include?([position[0], position[1]])
+      end
     end
       'white'
-    elsif white_pieces.any? do |piece|
-      piece.possible_moves.include?([black_king.row, black_king.column])
+    elsif black_king.possible_moves.all? do |position|
+      white_pieces.any? do |piece|
+        piece.possible_moves.include?(position[0], position[1])
+      end
     end
       'black'
     end
   end
 
-  def checkmate?
-    white_king = white_pieces.find { |piece| piece.instance_of?(King) }
-    black_king = black_pieces.find { |piece| piece.instance_of?(King) }
+  def end_game
+    white_player.win_message if checkmate? == 'black'
+    black_player.win_message if checkmate? == 'white'
+    play_again?
   end
 
-  def end_game
+  def play_again?
+    response = white_player.play_again_message
+    if response
+      Game.new.play_game
+    else
+      puts 'Thanks for playing!'
+    end
   end
 end
